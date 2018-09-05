cd885cf047502b4a507326be4ce5598c0a1d455e
Yura Zatsepin <yura.zatsepin@gmail.com>
2018-09-05 17:34:18 +0300

    Update styles

--- src/App.js
+++ src/App.js
@@ -1,20 +1,59 @@
 import React from 'react'
+import { getGATimeseries } from './getGithubActivity'
+import LineChart from 'react-svg-line-chart'
+import 'normalize.css/normalize.css'
 import './App.css'
+
+class GATimeseries extends React.Component {
+  state = {
+    timeseries: []
+  }
+
+  componentDidMount() {
+    getGATimeseries({slug: this.props.slug}).then(({data}) => {
+      this.setState({ timeseries: data.githubActivity })
+    })
+  }
+
+  render() {
+    const { children, render } = this.props
+    const timeseries = this.state.timeseries.map((el, index) => ({x:index, y: el.activity})) || []
+    if (typeof children === 'function') return (<div>{children(timeseries)}</div>)
+    return render(timeseries)
+  }
+}
 
 const GithubActivityList = ({activities = []}) => (
   <table>
+    <thead>
+      <tr>
+       <td>Last 30 days activity</td>
+      </tr>
+    </thead>
     <tbody>
-    <tr>
-      <th>name</th>
-      <th>30 days dev activity</th>
-    </tr>
-    {activities.map(activity => (
+    {activities.map((activity, index) => (
       <tr key={activity.id}>
         <td>
-          <a href={activity.githubLink}>{activity.slug}</a> 
+          {index + 1}. <a href={activity.githubLink}>{activity.slug}</a> 
         </td>
         <td>
-          {activity.activity} 
+          <div className='rotate'>
+            <label>{activity.activity}</label>
+          </div>
+        </td>
+        <td className='chart'>
+          <GATimeseries slug={activity.slug}>
+            {timeseries => (
+              <LineChart 
+                data={timeseries}
+                pointsRadius={0} 
+                pathWidth={8}
+                gridVisible={false}
+                axisVisible={false}
+                labelsVisible={false}
+              />
+            )}
+          </GATimeseries>
         </td>
       </tr>
     ))}
@@ -22,23 +61,56 @@ const GithubActivityList = ({activities = []}) => (
   </table>
 )
 
+const MonthControl = () => (
+  <div className='month-control'>
+    <strong>2018</strong>
+    <div className='month active'>September</div>
+    <div className='month'>August</div>
+    <div className='month'>July</div>
+    <div className='month'>June</div>
+  </div>
+)
+
 const App = ({activities = []}) => (
   <div className='container'>
-    <header>
-      <h1>Top 10 crypto projects <br /> by development activity</h1>
+    <header className='title'>
+      <h2>Top 12 crypto projects by 30 days github activity</h2>
     </header>
-    <GithubActivityList activities={activities}/>
+    {
+      //<MonthControl />
+    }
+    <section>
+      <div className='github-activity-explanation'>
+        <div className='rotate'>
+          <h2><span>Top 12 crypto</span></h2>
+          <h2><span>projects</span></h2>
+          <h2><span>by github</span></h2>
+          <h2><span>activity</span></h2>
+        </div>
+        <p>
+          <strong>Github</strong> activity is the number of <strong>Github</strong> 'events' including issue interactions,
+          pull requests, comments, and wiki edits, plus the number of public
+          repositories a project is maintaining.</p>
+        <p>
+          Read <a href="https://medium.com/santiment/tracking-github-activity-of-crypto-projects-introducing-a-better-approach-9fb1af3f1c32">
+            more about how it works?
+          </a>
+        </p>
+      </div>
+      <GithubActivityList activities={activities}/>
+      {
+      //<div>
+        //<p>Subscribe to get monthly notification about top projects</p>
+        //<input type="" /><button>Subscribe</button>
+      //</div>
+      }
+    </section>
     <footer>
-      <p>-------</p>
-      <h3>What 'development activity' is?</h3>
-      <p>Metric based on number of Github 'events' including issue interactions,
-        pull requests, comments, and wiki edits, plus the number of public
-        repositories a project is maintaining</p>
-      <p>Read <a href="https://medium.com/santiment/tracking-github-activity-of-crypto-projects-introducing-a-better-approach-9fb1af3f1c32">
-          more about how it works?
-        </a>
-      </p>
-      <small>Made by <a href='https://santiment.net'>Santiment API</a></small>
+      <small style={{
+        float: 'right',
+        marginBottom: 22
+      }}>Made by <a href='https://santiment.net'>Santiment API</a>
+      </small>
     </footer>
   </div>
 )
--- src/getGithubActivity.js
+++ src/getGithubActivity.js
@@ -9,13 +9,55 @@ const mapDataToActivities = ({data = { allProjects: [] }}) =>
   [...data.allProjects]
     .sort((a, b) => a.averageDevActivity - b.averageDevActivity)
     .reverse()
-    .filter((_, index) => index < 10)
+    .filter((_, index) => index < 12)
     .map(activity => ({
       ...activity,
       activity: activity.averageDevActivity.toFixed(2)
     }))
 
-export default function getGithubActivity() {
+const getXMonthAgo = (xMonthAgo = 6) => {
+  const now = new Date()
+  now.setMonth(now.getMonth() - xMonthAgo)
+  return now.toISOString()
+}
+
+export const getGATimeseries = ({slug = 'ethereum', interval = 1}) => {
+  return client
+    .query({
+      query: gql`
+        query queryGithubActivity(
+          $slug: String
+          $from: DateTime
+          $to: DateTime
+          $interval: String
+          $transform: String
+          $movingAverageIntervalBase: String
+        ) {
+          githubActivity(
+            slug: $slug
+            from: $from
+            to: $to
+            interval: $interval
+            transform: $transform
+            movingAverageIntervalBase: $movingAverageIntervalBase
+          ) {
+            datetime
+            activity
+          }
+        }
+      `,
+      variables: {
+        slug,
+        from: getXMonthAgo(interval),
+        to: (new Date()).toISOString(),
+        interval: '',
+        transform: 'movingAverage',
+        movingAverageIntervalBase: '1w'
+      }
+    })
+}
+
+export const getGithubActivity = () => {
   return client
     .query({
       query: gql`
@@ -31,3 +73,5 @@ export default function getGithubActivity() {
     })
     .then(mapDataToActivities)
 }
+
+export default getGithubActivity
