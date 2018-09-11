1a54770ecaaf4be404fa3cc148bdc6296549e7e5
Yura Zatsepin <yura.zatsepin@gmail.com>
2018-09-11 18:19:20 +0300

    Add normalize.css

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import App from './App'
 import getGithubActivity from './getGithubActivity'
 import registerServiceWorker from './registerServiceWorker'
+import 'normalize.css/normalize.css'
+import './index.css'
 
 getGithubActivity().then(activities => {
   ReactDOM.render(<App activities={activities} />, document.getElementById('root'))
